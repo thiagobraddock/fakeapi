ae4bfee9834bf779f1f8346022a43a6d5562401c
Thiago "BRADDOCK" Oliveira <braddock01@hotmail.com>
2023-08-02 15:13:07 -0300

    fix: db.json format

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+const port = process.env.PORT || 3001;
+
+// Print out the available routes
+console.log('Available Routes:');
+console.log(JSON.stringify(router.db.getState(), null, 2));
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
